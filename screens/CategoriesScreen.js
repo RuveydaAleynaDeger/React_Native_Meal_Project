6e739d6ea2b547f04e5ef8b4ef23e3704d4747c7
RuveydaAleynaDeger <degerece29@gmail.com>
2021-07-03 17:30:43 +0300

    bottom tabs, material bottom yapıldı.

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,31 +1,53 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet,TouchableOpacity } from 'react-native';
-import {CATEGORIES} from '../data/dummy-data';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
 
-const renderGridItem = (itemData) =>{
-
-  return <View style={styles.gridItem}><Text>{itemData.item.title}</Text></View>;
-};
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
+  const renderGridItem = itemData => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+            params: {
+              categoryId: itemData.item.id
+            }
+          });
+        }}
+      />
+    );
+  };
+
   return (
-   <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 };
 
-const styles= StyleSheet.create({
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories'
+};
 
-    screen:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    },
-    gridItem:{
-      flex:1,
-      margin:15,
-      height:150
-    }
-
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default CategoriesScreen;
